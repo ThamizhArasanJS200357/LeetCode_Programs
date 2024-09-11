d5f98992eb117e31baf0bb7497b13a304804c61b
ThamizhArasanJS200357 <thamizharasan2003@gmail.com>
2024-09-11 14:37:49 +0530

    Binary Seacrh

---
+++ Level-1/Binary_Search.java
@@ -0,0 +1,47 @@
+public class Binary_Search {
+    public static int search(int []nums, int target) {
+        // Write your code here.
+        int l=0;
+        int r=nums.length-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(nums[mid]>target){
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        return -1;
+
+    }
+}
+
+
+// Sample Input 1:
+// 7
+// 1 3 7 9 11 12 45
+// 3
+// Sample Output 1:
+// 1
+// Explanation of sample output 1:
+// nums = [1, 3, 7, 9, 11, 12, 45],
+// The index of element '3' is 1.
+// Hence, the answer is '1'.
+
+
+// Sample Input 2:
+// 7
+// 1 2 3 4 5 6 7
+// 9
+// Sample Output 2:
+// -1
+// Explanation of sample output 2:
+// nums = [1, 2, 3, 4, 5, 6, 7],
+// Element '9' doesn't exist.
+// Hence, the answer is '-1'.
+
+// LINK:https://www.naukri.com/code360/problems/binary-search_972?interviewProblemRedirection=true&practice_topic%5B%5D=Arrays&practice_topic%5B%5D=Dynamic%20Programming&difficulty%5B%5D=Easy&leftPanelTabValue=PROBLEM
