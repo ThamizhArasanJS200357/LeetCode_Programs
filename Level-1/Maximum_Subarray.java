592c092fa1ba0ae61f73075c9ace841ef0d7fa8c
ThamizhArasanJS200357 <thamizharasan2003@gmail.com>
2024-09-16 14:19:04 +0530

    Maximum Subarray

---
+++ Level-1/Maximum_Subarray.java
@@ -0,0 +1,22 @@
+import java.util.* ;
+import java.io.*; 
+
+public class Maximum_Subarray {
+	
+	public static long maxSubarraySum(int[] arr, int n) {
+		// write your code here
+		long max=0;
+		long sum=0;
+		for(int i=0;i<n;i++){
+			sum+=arr[i];
+			if(sum>max){
+				max=sum;
+			}
+			if(sum<0){
+				sum=0;
+			}
+		}
+		return max;
+	}
+
+}
